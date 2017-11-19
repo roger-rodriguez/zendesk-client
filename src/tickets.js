0b81cb6b2cfbd0aac7fe98beb8295e1070d396dc
Roger Rodriguez <roger_1591@me.com>
2017-11-19 18:03:50 -0500

    removing un-used code

--- src/tickets.js
+++ src/tickets.js
@@ -2,9 +2,6 @@ import fetch from './fetch';
 
 export default (options) =>{
 
-  // console.log('Tickets options');
-  // console.log(options);
-
   return {
     create : create.bind(null, options),
     list   : list.bind(null, options),
@@ -43,32 +40,3 @@ const list = (options, b) =>{
   console.log('Tickets list()');
   console.log(options, b);
 };
-
-
-// export default {
-//
-//   create : (ticket) =>{
-//     console.log('create');
-//     console.log(ticket);
-//     console.log(this);
-//   },
-//
-//
-// }
-
-// export default class Tickets{
-//
-//   constructor(options){
-//     console.log('tickets constructor');
-//     console.log(options);
-//     this.options = options;
-//   }
-//
-//   create(ticket){
-//     console.log('create');
-//     console.log(ticket);
-//     console.log(this.options);
-//   }
-//
-// }
-
